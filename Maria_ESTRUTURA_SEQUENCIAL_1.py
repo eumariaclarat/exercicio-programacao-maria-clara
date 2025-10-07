df37e8a3a3a86a88b0dc251bd81c4e08f9b0bfdf
eumariaclarat <maria_c_torres@estudante.sesisenai.org.br>
2025-10-07 20:28:40 -0300

    5

--- Maria_ESTRUTURA_SEQUENCIAL_1.py
+++ Maria_ESTRUTURA_SEQUENCIAL_1.py
@@ -1,6 +1,3 @@
-nota_1 = float(input("Informe sua nota 1:"))
-nota_2 = float(input("Informe sua nota 2:"))
-nota_3 = float(input("Informe sua nota 3:"))
-nota_4 = float(input("Informe sua nota 4:"))
-media = (nota_1 + nota_2 + nota_3 + nota_4)/4
-print("Média",media:)
+metros = float(input("Converta de metros para centímetros:"))
+centimetros = metros * 100
+print("centimetros :",centimetros)
