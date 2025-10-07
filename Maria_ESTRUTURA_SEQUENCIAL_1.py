99da411aaee653a0b42390f79d4c13fb4631b111
eumariaclarat <maria_c_torres@estudante.sesisenai.org.br>
2025-10-07 20:14:59 -0300

    Create Maria_ESTRUTURA_SEQUENCIAL_1.py

---
+++ Maria_ESTRUTURA_SEQUENCIAL_1.py
@@ -0,0 +1,6 @@
+nota_1 = float(input("Informe sua nota 1:"))
+nota_2 = float(input("Informe sua nota 2:"))
+nota_3 = float(input("Informe sua nota 3:"))
+nota_4 = float(input("Informe sua nota 4:"))
+media = (nota_1 + nota_2 + nota_3 + nota_4)/4
+print("Média",media:)
