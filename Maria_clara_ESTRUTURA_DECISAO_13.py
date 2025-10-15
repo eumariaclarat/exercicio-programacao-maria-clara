0392d3a79ed8fea2769d6b30a9235d06a1521698
eumariaclarat <maria_c_torres@estudante.sesisenai.org.br>
2025-10-14 21:54:43 -0300

    MC

--- Maria_clara_ESTRUTURA_DECISAO_13.py
+++ Maria_clara_ESTRUTURA_DECISAO_13.py
@@ -1,6 +1,6 @@
 #Faça um programa que leia um número e exiba o dia correspondente da semana.
 # (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
-#desenvolvido por: Maria Clara Marinho Torres 
+#desenvolvido por: Maria Clara Marinho Torres
 
 semana = input("Qual número da semana?(1 a 7).")
 #se
