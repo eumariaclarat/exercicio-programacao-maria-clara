507535f23c99762679b6384da0497a7f002310ac
eumariaclarat <maria_c_torres@estudante.sesisenai.org.br>
2025-10-22 21:44:04 -0300

    c

---
+++ Maria_ESTRUTURA_REPETICAO_14.py
@@ -0,0 +1,14 @@
+#Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade de números pares e a quantidade de números impares.
+#desenvolvido por: Maria Clara Marinho Torres
+
+pares = 0
+impares = 0
+for i in range(10):
+    numero = int(input(f"Digite o {i+1}º número inteiro: "))
+    
+    if numero % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+print("Quantidade de números pares:",pares)
+print("Quantidade de números ímpares:",impares)
